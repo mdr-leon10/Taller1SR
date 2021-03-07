4c1e54e5db443a05563bb94db63368126ed0ed5a
Jorge Gomez <jagv1998@gmail.com>
2021-03-07 14:46:00 -0500

    delcaration order

--- Taller1/api/models.py
+++ Taller1/api/models.py
@@ -7,15 +7,15 @@ class User(models.Model):
 	is_old_user = models.BooleanField(default=False)
 	recommendation_frame = models.IntegerField(default=0)
 
-class Interactions(models.Model):
-	identifier = models.AutoField(primary_key=True)
-	from_song = models.ForeignKey(Songs, on_delete=models.CASCADE)
-	from_user = models.ForeignKey(User, on_delete=models.CASCADE)
-	count = models.PositiveIntegerField()
-
 class Songs(models.Model):
 	identifier = models.AutoField(primary_key=True)
 	artist_id = models.CharField(max_length=255)
 	artist_name = models.CharField(max_length=255)
 	track_name = models.CharField(max_length=255)
 	track_id = models.CharField(max_length=255)
+
+class Interactions(models.Model):
+	identifier = models.AutoField(primary_key=True)
+	from_song = models.ForeignKey(Songs, on_delete=models.CASCADE)
+	from_user = models.ForeignKey(User, on_delete=models.CASCADE)
+	count = models.PositiveIntegerField()
