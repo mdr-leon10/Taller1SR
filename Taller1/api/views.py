460ab7104e82c2ba838d955ebc1a6acd72b2196c
Jorge Gomez <jagv1998@gmail.com>
2021-03-07 11:25:41 -0500

    return errors from serializer

--- Taller1/api/views.py
+++ Taller1/api/views.py
@@ -29,12 +29,10 @@ def get_user_data(request, user_query_id):
 	try:
 		user = User.objects.get(user_id = user_query_id)
 		serialized_entity = UserSerializer(data=user)
-		print(user)
-		print(serialized_entity.data)
 		if serialized_entity.is_valid():
 			return JsonResponse(JSONRenderer().render(serialized_entity.data), safe=False,status=status.HTTP_200_OK)
 		else:
-			return JsonResponse('Internal server error, data not valid', safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+			return JsonResponse(serialized_entity.errors, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 	except User.DoesNotExist:
 		return JsonResponse('Not found', safe=False,status=status.HTTP_404_NOT_FOUND)
 
