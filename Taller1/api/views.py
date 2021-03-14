f5be4400410cc429b2c8e55a5fd3c0eb8c20b2c9
Jorge Gomez <jagv1998@gmail.com>
2021-03-13 22:02:40 -0500

    default for empty

--- Taller1/api/views.py
+++ Taller1/api/views.py
@@ -229,7 +229,9 @@ def get_artists_with_filter(request):
 	query_dict = request.GET.dict()
 	artist_name_prefix = '' if 'artist_name_prefix' not in query_dict else query_dict['artist_name_prefix']
 	try:
-		artists_query = Songs.objects.filter(artist_name__contains=artist_name_prefix)
+		artist_query = Songs.objects.all()
+		if artist_name_prefix != '':
+			artists_query = artist_query.filter(artist_name__contains=artist_name_prefix)
 		artists_search = ArtistSearchSerializer(artists_query[0:min(len(artists_query), 100)], many=True)
 		return JsonResponse(artists_search.data, safe=False, status=status.HTTP_200_OK)
 	except:
